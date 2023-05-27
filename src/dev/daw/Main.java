390cf65aaef5137c09b64920d8077ea87d60afb8
Dawid Rybinski <42513264+dawid3201@users.noreply.github.com>
2023-05-27 14:52:46 +0100

    First commit

---
+++ src/dev/daw/Main.java
@@ -0,0 +1,23 @@
+package dev.daw;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
+//Mappable interface with one abstract method "render"
+
+//2 classes Point and Line that implements this interface
+
+//2 specific classes that extend each of these, for mappable item of interest
+
+//example national park can be points and rivers will be lines
+
+//Park class that extends Point and River class that extends river so, line will only accept River type and locations will only accept Point type
+
+//Constructor or methods to add to classes, attributes: Name and Googled Location
+
+//generic class Layer <T>, only allow mappable elements as that type 1) Single Private field 2) List of elements to be mapped
